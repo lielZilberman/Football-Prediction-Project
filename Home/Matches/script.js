76dfadd6a8a94df43feabb1cbe216e069dc00a9a
lielZilberman <lielzilberman@gmail.com>
2023-04-22 16:56:04 +0300

    added prediction button and modal

--- Home/Matches/script.js
+++ Home/Matches/script.js
@@ -1,5 +1,23 @@
 const rect = document.querySelector(".rect");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const htmlBtn = `<div class="btn">Our Prediction</div>`;
 let today = new Date();
+
+const openModal = function () {
+  // document.body.style.position = "fixed";
+  modal.style.top = `${window.scrollY + 270}px`;
+  overlay.style.top = `${window.scrollY}px`;
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  document.body.classList.add(".stop-scrolling");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
 const options = {
   method: "GET",
@@ -21,8 +39,8 @@ fetch(
       (${response.events[i].status.description})</p>`;
       rect.insertAdjacentHTML("beforeend", htmlClubs);
       const htmlScore = `<p class= "score">${
-        response.events[i].homeScore.display
-          ? response.events[i].homeScore.display
+        response.events[i].awayScore.display
+          ? response.events[i].awayScore.display
           : 0
       } - ${
         response.events[i].homeScore.display
@@ -32,9 +50,16 @@ fetch(
       document
         .querySelector(`.c${i}`)
         .insertAdjacentHTML("beforeend", htmlScore);
+      document.querySelector(`.c${i}`).insertAdjacentHTML("beforeend", htmlBtn);
     }
   })
-  .catch((err) => console.error(err));
+  .catch((err) => console.error(err))
+  .finally(() => {
+    for (let i = 0; i < document.querySelectorAll(".btn").length; i++)
+      document.querySelectorAll(".btn")[i].addEventListener("click", openModal);
+
+    btnCloseModal.addEventListener("click", closeModal);
+  });
 // fetch("https://footapi7.p.rapidapi.com/api/team/2672/image", options)
 //   .then((response) => response.json())
 //   .then((response) => console.log(response))
