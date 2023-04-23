49ae7f552fa74b12aeb41e26d24e3122dd23857c
lielZilberman <lielzilberman@gmail.com>
2023-04-23 23:50:38 +0300

    added time and fixed modal bug

--- Home/Matches/script.js
+++ Home/Matches/script.js
@@ -4,6 +4,22 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const htmlBtn = `<div class="btn">Our Prediction</div>`;
 let today = new Date();
+let isStarted = false;
+
+const dateFormat = (date) => {
+  const currDate = new Date(date * 1000);
+  return currDate;
+};
+
+const timeFormat = (time) => {
+  if (time / 10 < 1) return `0${time}`;
+  return time;
+};
+
+const scrollModal = () => {
+  modal.style.top = `${window.scrollY + 270}px`;
+  overlay.style.top = `${window.scrollY}px`;
+};
 
 const openModal = function () {
   // document.body.style.position = "fixed";
@@ -12,6 +28,7 @@ const openModal = function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
   document.body.classList.add(".stop-scrolling");
+  window.addEventListener("scroll", scrollModal);
 };
 
 const closeModal = function () {
@@ -34,23 +51,33 @@ fetch(
 )
   .then((response) => response.json())
   .then((response) => {
+    console.log(response);
     for (let i = 0; i < 200; i++) {
+      if (
+        response.events[i].status.description != "Not started" &&
+        response.events[i].status.description != "Postponed"
+      ) {
+        isStarted = true;
+      }
       const htmlClubs = `<p class="clubs c${i}">${response.events[i].awayTeam.name} vs ${response.events[i].homeTeam.name}
       (${response.events[i].status.description})</p>`;
       rect.insertAdjacentHTML("beforeend", htmlClubs);
       const htmlScore = `<p class= "score">${
-        response.events[i].awayScore.display
+        isStarted
           ? response.events[i].awayScore.display
-          : 0
-      } - ${
-        response.events[i].homeScore.display
+          : timeFormat(dateFormat(response.events[i].startTimestamp).getHours())
+      } ${isStarted ? "-" : ":"} ${
+        isStarted
           ? response.events[i].homeScore.display
-          : 0
+          : timeFormat(
+              dateFormat(response.events[i].startTimestamp).getMinutes()
+            )
       }</p>`;
       document
         .querySelector(`.c${i}`)
         .insertAdjacentHTML("beforeend", htmlScore);
       document.querySelector(`.c${i}`).insertAdjacentHTML("beforeend", htmlBtn);
+      isStarted = false;
     }
   })
   .catch((err) => console.error(err))
