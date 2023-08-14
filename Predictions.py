f9a2a3fdf5942d730c53d654a30ee50f17055729
itzikbs1 <92460446+itzikbs1@users.noreply.github.com>
2023-08-14 13:23:33 +0300

    Update Predictions.py

--- Predictions.py
+++ Predictions.py
@@ -1,15 +1,5 @@
 import pandas as pd
-import xgboost as xgb
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import f1_score, confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
-from IPython.display import display
-from os import path, makedirs, walk
-from sklearn.preprocessing import scale, LabelEncoder
-from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
+from HandleData import HandleData
 
 en_data_folder = 'english-premier-league_zip'
 es_data_folder = 'spanish-la-liga_zip'
@@ -19,120 +9,96 @@ it_data_folder = 'italian-serie-a_zip'
 
 data_folders = [en_data_folder, es_data_folder,
                 fr_data_folder, ge_data_folder, it_data_folder]
-season_range = (9, 22)
 en_data = []
 es_data = []
 fr_data = []
 ge_data = []
 it_data = []
 
+HandleData = HandleData()
 
-def org_data(data, data_folder):
-    for season in range(season_range[0], season_range[1] + 1):
-        data.append(
-            'data/{}/data/season-{:02d}{:02d}_csv.csv'.format(data_folder, season, season + 1))
-    data_frame = []
-    for data_file in data:
-        if path.exists(data_file):
-            data_frame.append(pd.read_csv(data_file))
-    return pd.concat(data_frame, ignore_index=True)
+en_merge_data = HandleData.org_data(en_data, en_data_folder)
+es_merge_data = HandleData.org_data(es_data, es_data_folder)
+ge_merge_data = HandleData.org_data(ge_data, ge_data_folder)
+fr_merge_data = HandleData.org_data(fr_data, fr_data_folder)
+it_merge_data = HandleData.org_data(it_data, it_data_folder)
 
+en_res16, en_table16, en_feature_table = HandleData.compute(en_merge_data)
+es_res16, es_table16, es_feature_table = HandleData.compute(es_merge_data)
+ge_res16, ge_table16, ge_feature_table = HandleData.compute(ge_merge_data)
+fr_res16, fr_table16, fr_feature_table = HandleData.compute(fr_merge_data)
+it_res16, it_table16, it_feature_table = HandleData.compute(it_merge_data)
 
-en_merge_data = org_data(en_data, en_data_folder)
-es_merge_data = org_data(es_data, es_data_folder)
-ge_merge_data = org_data(ge_data, ge_data_folder)
-fr_merge_data = org_data(fr_data, fr_data_folder)
-it_merge_data = org_data(it_data, it_data_folder)
+en_feature_table = HandleData.add_features(en_feature_table, en_table16)
+es_feature_table = HandleData.add_features(es_feature_table, es_table16)
+gr_feature_table = HandleData.add_features(ge_feature_table, ge_table16)
+fr_feature_table = HandleData.add_features(fr_feature_table, fr_table16)
+it_feature_table = HandleData.add_features(it_feature_table, it_table16)
 
-input_cols = ['home_encoded', 'away_encoded', 'HS', 'AS', 'HST', 'AST']
-# input_cols = ['home_encoded', 'away_encoded', 'HTHG', 'HTAG', 'HS',
-#                 'AS', 'HST', 'AST', 'HR', 'AR']
-output_cols = ['FTR']
-all_cols = input_cols + output_cols
+en_feature_table["Result"] = en_feature_table.apply(lambda row: HandleData.transformResult(row), axis=1)
+es_feature_table["Result"] = es_feature_table.apply(lambda row: HandleData.transformResult(row), axis=1)
+ge_feature_table["Result"] = ge_feature_table.apply(lambda row: HandleData.transformResult(row), axis=1)
+fr_feature_table["Result"] = fr_feature_table.apply(lambda row: HandleData.transformResult(row), axis=1)
+it_feature_table["Result"] = it_feature_table.apply(lambda row: HandleData.transformResult(row), axis=1)
 
+en_final = en_res16.sort_index(ascending=False)
+es_final = es_res16.sort_index(ascending=False)
+ge_final = ge_res16.sort_index(ascending=False)
+fr_final = fr_res16.sort_index(ascending=False)
+it_final = it_res16.sort_index(ascending=False)
 
-def process_data(data):
-    encoder = LabelEncoder()
-    home_encoded = encoder.fit_transform(data['HomeTeam'])
-    home_encoded_mapping = dict(
-        zip(encoder.classes_, encoder.transform(encoder.classes_).tolist()))
-    data['home_encoded'] = home_encoded
+en_final = en_final[['HomeTeam', 'AwayTeam', 'FTR', 'FTHG', 'FTAG', 'HS', 'AS', 'HST', 'AST', 'HC', 'AC']]
+es_final = es_final[['HomeTeam', 'AwayTeam', 'FTR', 'FTHG', 'FTAG', 'HS', 'AS', 'HST', 'AST', 'HC', 'AC']]
+ge_final = ge_final[['HomeTeam', 'AwayTeam', 'FTR', 'FTHG', 'FTAG', 'HS', 'AS', 'HST', 'AST', 'HC', 'AC']]
+fr_final = fr_final[['HomeTeam', 'AwayTeam', 'FTR', 'FTHG', 'FTAG', 'HS', 'AS', 'HST', 'AST', 'HC', 'AC']]
+it_final = it_final[['HomeTeam', 'AwayTeam', 'FTR', 'FTHG', 'FTAG', 'HS', 'AS', 'HST', 'AST', 'HC', 'AC']]
+# Adding next week fixtures
+en_new_fixtures = pd.DataFrame([['Southampton', 'Liverpool', 'D', 0, 0, 0, 0, 0, 0, 0, 0]], columns=en_final.columns)
+es_new_fixtures = pd.DataFrame([['Valladolid', 'Getafe', 'D', 0, 0, 0, 0, 0, 0, 0, 0]], columns=es_final.columns)
+ge_new_fixtures = pd.DataFrame([['Wolfsburg', 'Hertha', 'D', 0, 0, 0, 0, 0, 0, 0, 0]], columns=ge_final.columns)
+fr_new_fixtures = pd.DataFrame([['Troyes', 'Lille', 'D', 0, 0, 0, 0, 0, 0, 0, 0]], columns=fr_final.columns)
+it_new_fixtures = pd.DataFrame([['Udinese', 'Juventus', 'D', 0, 0, 0, 0, 0, 0, 0, 0]], columns=it_final.columns)
 
-    encoder = LabelEncoder()
-    away_encoded = encoder.fit_transform(data['AwayTeam'])
-    away_encoded_mapping = dict(
-        zip(encoder.classes_, encoder.transform(encoder.classes_).tolist()))
-    data['away_encoded'] = away_encoded
-    data = data[all_cols]
+en_final = HandleData.new_table(en_new_fixtures, en_final)
+es_final = HandleData.new_table(es_new_fixtures, es_final)
+ge_final = HandleData.new_table(ge_new_fixtures, ge_final)
+fr_final = HandleData.new_table(fr_new_fixtures, fr_final)
+it_final = HandleData.new_table(it_new_fixtures, it_final)
 
-    data = data.dropna(axis=0)
-    return data
+en_final, en_n_games, en_num_games = HandleData.handle(en_final, en_table16)
+es_final, es_n_games, es_num_games = HandleData.handle(es_final, es_table16)
+ge_final, ge_n_games, ge_num_games = HandleData.handle(ge_final, ge_table16)
+fr_final, fr_n_games, fr_num_games = HandleData.handle(fr_final, fr_table16)
+it_final, it_n_games, it_num_games = HandleData.handle(it_final, it_table16)
 
-
-en_merge_data = process_data(en_merge_data)
-es_merge_data = process_data(es_merge_data)
-ge_merge_data = process_data(ge_merge_data)
-fr_merge_data = process_data(fr_merge_data)
-it_merge_data = process_data(it_merge_data)
-
-
-# print(it_merge_data)
-
-
-def train_classifier(clf, X_train, y_train):
-    clf.fit(X_train, y_train)
-
-
-def predict_labels(clf, features, target):
-    y_pred = clf.predict(features)
-    acc = sum(target == y_pred) / float(len(y_pred))
-    return f1_score(target, y_pred, average='micro'), acc
-
-
-def model(clf, X_train, y_train, X_test, y_test):
-    le = LabelEncoder()
-    y_train = le.fit_transform(y_train)
-    y_test = le.transform(y_test)
-
-    train_classifier(clf, X_train, y_train)
-
-    f1, acc = predict_labels(clf, X_train, y_train)
-    print("Training Info:")
-    print("-" * 20)
-    print("F1 Score:{}".format(f1))
-    print("Accuracy:{}".format(acc))
-    f1, acc = predict_labels(clf, X_test, y_test)
-    print("Test Metrics:")
-    print("-" * 20)
-    print("F1 Score:{}".format(f1))
-    print("Accuracy:{}".format(acc))
-    print("Confusion Matrix:")
-    print(confusion_matrix(y_test, clf.predict(X_test, )))
+en_X_train, en_X_test, en_y_train, en_y_test, en_X_predict = HandleData.split(en_final, en_n_games, en_num_games)
+es_X_train, es_X_test, es_y_train, es_y_test, es_X_predict = HandleData.split(es_final, es_n_games, es_num_games)
+ge_X_train, ge_X_test, ge_y_train, ge_y_test, ge_X_predict = HandleData.split(ge_final, ge_n_games, ge_num_games)
+fr_X_train, fr_X_test, fr_y_train, fr_y_test, fr_X_predict = HandleData.split(fr_final, fr_n_games, fr_num_games)
+it_X_train, it_X_test, it_y_train, it_y_test, it_X_predict = HandleData.split(it_final, it_n_games, it_num_games)
 
 
-Xen = en_merge_data[input_cols]
-print(Xen)
-Yen = en_merge_data['FTR']
+print("English accuracy results:")
+HandleData.train(en_X_train, en_X_test, en_y_train, en_y_test)
+print("Spanish accuracy results:")
+HandleData.train(es_X_train, es_X_test, es_y_train, es_y_test)
+print("German accuracy results:")
+HandleData.train(ge_X_train, ge_X_test, ge_y_train, ge_y_test)
+print("France accuracy results:")
+HandleData.train(fr_X_train, fr_X_test, fr_y_train, fr_y_test)
+print("Italy accuracy results:")
+HandleData.train(it_X_train, it_X_test, it_y_train, it_y_test)
 
-Xen_train, Xen_test, Yen_train, Yen_test = train_test_split(Xen, Yen, test_size=0.2, random_state=2, stratify=Yen)
 
-svc_classifier = SVC(random_state=100, kernel='rbf')
-lr_classifier = LogisticRegression(multi_class='ovr', max_iter=500)
-nbClassifier = GaussianNB()
-rfClassifier = RandomForestClassifier()
+en_res = HandleData.test_results(en_X_predict, en_final, en_num_games)
+es_res = HandleData.test_results(es_X_predict, es_final, es_num_games)
+ge_res = HandleData.test_results(ge_X_predict, ge_final, ge_num_games)
+fr_res = HandleData.test_results(fr_X_predict, fr_final, fr_num_games)
+it_res = HandleData.test_results(it_X_predict, it_final, it_num_games)
 
-##
-print()
-print("Logistic Regression one vs All Classifier")
-print("-" * 20)
-model(lr_classifier, Xen_train, Yen_train, Xen_test, Yen_test)
 
-print()
-print("Gaussain Naive Bayes Classifier")
-print("-" * 20)
-model(nbClassifier, Xen_train, Yen_train, Xen_test, Yen_test)
-
-print()
-print("Random Forest Classifier")
-print("-" * 20)
-model(rfClassifier, Xen_train, Yen_train, Xen_test, Yen_test)
+HandleData.print_results(en_res)
+HandleData.print_results(es_res)
+HandleData.print_results(ge_res)
+HandleData.print_results(fr_res)
+HandleData.print_results(it_res)
